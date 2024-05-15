260a0ef17cab22aecadb7c551d451ee5ac0f30ac
MATHIS LAMBERT <mathis.lambert27@gmail.com>
2024-05-15 15:33:35 +0200

    [+] route

--- front_app/src/app/socket.ts
+++ front_app/src/app/socket.ts
@@ -6,6 +6,7 @@ const currentProtocol = window.location.protocol;
 
 const URL = import.meta.env.VITE_API_URL || `${currentProtocol}//${currentURL}`;
 
+console.log(import.meta.env.VITE_API_URL);
 console.log("URL", URL);
 
 const socket = io(URL, {
